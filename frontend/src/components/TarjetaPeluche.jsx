e1ee4aa2bbe2e8fde31c0023d02cfaa2de8100a8
erickferna29 <eflglv@hotmail.com>
2026-01-04 11:44:36 -0600

    Añadir arhivos css del proyecto original

--- frontend/src/components/TarjetaPeluche.jsx
+++ frontend/src/components/TarjetaPeluche.jsx
@@ -1,11 +1,16 @@
-//un componente es como una pieza de Lego que puedes reutilizar mil veces
-export function TarjetaPeluche({ nombre, precio, imagen }) {
+//un componente es como una pieza de Lego que se puede reutilizar miles de veces
+export function TarjetaPeluche({ producto }) {
   return (
-    <div className="productBox"> {/* Usamos tus clases de Style1.css */}
-      <img src={imagen} className="imageInBox" alt={nombre} />
-      <p className="text">{nombre}</p>
-      <p className="text">${precio}</p>
-      <button className="buyBtn">Añadir al Carrito</button>
+    <div className="productBox"> 
+      {/* Usamos la URL de la imagen que viene de tu MySQL */}
+      <img 
+        src={producto.Imagen} 
+        className="imageInBox" 
+        alt={producto.Nombre} 
+      />
+      <p className="text">{producto.Nombre}</p>
+      <p className="text">${Number(producto.Precio).toFixed(2)}</p>
+      <button className="buyBtn">Comprar</button>
     </div>
   );
 }
